fdddf3be65176730f452aadfef682afbe8c18e9c
Tiffany <tiffany831101@gmail.com>
2023-08-02 14:23:27 +0800

    feat: add weather api image and change page api port

--- page/src/api/apolloClient.js
+++ page/src/api/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/", // Replace with your actual GraphQL server endpoint
+  uri: "http://localhost:8081/", // Replace with your actual GraphQL server endpoint
   cache: new InMemoryCache(),
 });
 
