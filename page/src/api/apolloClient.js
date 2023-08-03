90f2e59bca4f512f1b80a4309463d52ec7ec03aa
Tiffany <tiffany831101@gmail.com>
2023-08-03 22:50:28 +0800

    feat: deploy yaml in k8s

--- page/src/api/apolloClient.js
+++ page/src/api/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:8081/", // Replace with your actual GraphQL server endpoint
+  uri: "http://localhost/api", // Replace with your actual GraphQL server endpoint
   cache: new InMemoryCache(),
 });
 
